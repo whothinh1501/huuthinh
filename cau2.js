d60c376df8cc84053afe677ee69d4a833f931db7
Nguyễn Trần Hữu Thịnh <92415386+whothinh1501@users.noreply.github.com>
2026-01-07 16:45:03 +0700

    Upload files for the first time

---
+++ cau2.js
@@ -0,0 +1,13 @@
+function doCau2() {
+    function isEven(n) {
+        return (n % 2 === 0) ? "số chẵn" : "số lẻ";
+    }
+
+    let n = Number(prompt("CÂU 2: Nhập số cần kiểm tra chẵn lẻ:"));
+    let ketQua = isEven(n);
+
+    document.write("<div class='exercise-box'>");
+    document.write("<h3>Kết quả Câu 2</h3>");
+    document.write("<p>Số bạn nhập: " + n + " là <b>" + ketQua + "</b></p>");
+    document.write("</div>");
+}
